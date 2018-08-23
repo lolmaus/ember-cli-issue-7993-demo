11239b36c721fe7c93d64d9e24ea280e0a1258bb
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2018-08-23 17:43:40 +0300

    Initial demo

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,7 +2,10 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import QUnit from 'qunit';
 
 setApplication(Application.create(config.APP));
+
+QUnit.done(() => console.log("~!@#$] If you see this in terminal or CI, then the issue does not happen. [$#@!~")); // eslint-disable-line no-console
 
 start();
